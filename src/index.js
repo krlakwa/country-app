bda3b65ebfab0fe6b281937251a52a5151bd7cd3
Krzysztof Łakwa <fenrir14@gmail.com>
2018-06-28 18:58:55 +0200

    Add navigation component do DOM tree

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory} from 'react-router';
 import { Provider } from 'react-redux';
 import DevTools from './DevTools';
 import store from './store/index';
 import { getCountries } from './actions/actions-countries';
+import Navigation from './presentational/navigation.component';
 
 render(
     <Provider store={store}>
         <div>
-            <h1>Project initialization</h1>
+            <Router history={hashHistory}>
+                <Route path='/' component={Navigation}>
+
+                </Route>
+            </Router>
             <DevTools />
         </div>
     </Provider>,
