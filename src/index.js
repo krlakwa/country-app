e999cfad77731d24c076fbab387f6a306621c118
Krzysztof Łakwa <fenrir14@gmail.com>
2018-06-28 14:43:35 +0200

    Add DevTools implementation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import DevTools from './DevTools';
 import store from './store/index';
+import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
-        <h1>Project initialization</h1>
+        <div>
+            <h1>Project initialization</h1>
+            <DevTools />
+        </div>
     </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries());
