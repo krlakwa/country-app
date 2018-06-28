da24b001c878d329bb56bc19de3c3708e4e838b9
Krzysztof Łakwa <fenrir14@gmail.com>
2018-06-28 19:04:19 +0200

    Remove Route import from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory} from 'react-router';
+import { Router, hashHistory} from 'react-router';
 import { Provider } from 'react-redux';
 import DevTools from './DevTools';
 import store from './store/index';
