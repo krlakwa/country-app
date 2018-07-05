bd1f19ffd5605361379f08b9de9e759a11a7527b
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-05 12:30:28 +0200

    Add delete-country case to reducer

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,4 +1,4 @@
-import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES } from '../actions/actions-countries';
+import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY } from '../actions/actions-countries';
 import countriesData from '../data/countries.json';
 
 const initialState = {
@@ -17,6 +17,10 @@ const countriesReducer = function(state = initialState, action) {
         case SEARCH_COUNTRIES:
             const foundCountries = state.countries.filter(country => country.name.toLowerCase().includes(action.searchText.toLowerCase()));
             return Object.assign({}, state, {visibleCountries: foundCountries});
+        case DELETE_COUNTRY:
+            const notDeletedCoutries = state.countries.filter(country => country.id !== action.id);
+            const notDeletedVisibleCountries = state.visibleCountries.filter(country => country.id !== action.id);
+            Object.assign({}, state, {countries: notDeletedCoutries, visibleCountries: notDeletedVisibleCountries});
         default:
             return state;
     }
