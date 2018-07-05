8f683bbad2255ced706a587be1df6866c6cec54e
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-05 09:43:16 +0200

    Add flag component and flag-list component

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+import style from './country.css';
 import Navigation from './presentational/navigation.component';
 import Home from './presentational/home.component';
 import Contact from './presentational/contact.component';
 import NotFound from './presentational/not-found.component'
 
 export default (
-    <Route path='/' component={Navigation}>
+    <Route style={style} path='/' component={Navigation}>
         <IndexRoute component={Home} />
         <Route path='contact' component={Contact} />
         <Route path='*' component={NotFound} />
