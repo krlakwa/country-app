64cf671d6d6ab29d941a188fbff2ba58e8d11fbc
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-05 13:20:55 +0200

    Add continents to routes

--- src/routes.js
+++ src/routes.js
@@ -7,14 +7,16 @@ import Contact from './presentational/contact.component';
 import NotFound from './presentational/not-found.component';
 import CountryFlagContainer from './containers/flag-container.component';
 import CountryDetailsContainer from './containers/country-detail-container.component';
+import ContinentsContainer from './containers/continents-containter.component';
 
 export default (
     <Route style={style} path='/' component={Navigation}>
         <IndexRoute component={Home} />
         <Route path='countries' >
             <IndexRoute component={CountryFlagContainer} />
-            <Route path="country/:id" component={CountryDetailsContainer} />
+            <Route path='country/:id' component={CountryDetailsContainer} />
         </Route>
+        <Route path='continents' component={ContinentsContainer} />
         <Route path='contact' component={Contact} />
         <Route path='*' component={NotFound} />
     </Route>
